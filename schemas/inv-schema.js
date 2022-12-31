d122cddced7a25910b932324617feb035bd99a9b
Aimilianos Dimitrak <76211331+AimilDimitrak@users.noreply.github.com>
2022-12-31 13:51:49 +0200

    Update inv-schema.js

--- schemas/inv-schema.js
+++ schemas/inv-schema.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
-const invSchema = mongoose.schema({
+const invSchema = mongoose.Schema({
     did: {
         required: true,
         type: Number,
     },
     obj: {
         required: true,
-        type: Array || Object,
+        type: Object,
     }
 })
 module.exports = mongoose.model('inv', invSchema)
